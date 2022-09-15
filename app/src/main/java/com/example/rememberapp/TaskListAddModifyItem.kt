a379de7d56167cf407823ba2e3c0ea73726f747b
Joeyphic <112992779+Joeyphic@users.noreply.github.com>
2022-09-14 22:28:08 -0400

    Add Layout and New Fragment to Navigation

---
+++ app/src/main/java/com/example/rememberapp/TaskListAddModifyItem.kt
@@ -0,0 +1,10 @@
+package com.example.rememberapp
+
+import androidx.fragment.app.Fragment
+import com.example.rememberapp.databinding.TaskListAddItemBinding
+
+class TaskListAddModifyItem : Fragment() {
+
+    private var _binding: TaskListAddItemBinding? = null
+
+}
