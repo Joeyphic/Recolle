d843fe627c40879e329a67bee621b2d783bf967e
Joeyphic <112992779+Joeyphic@users.noreply.github.com>
2022-11-08 22:00:28 -0500

    Hide Keyboard When EditText is Unfocused

--- app/src/main/java/com/example/rememberapp/TaskListAddModifyItem.kt
+++ app/src/main/java/com/example/rememberapp/TaskListAddModifyItem.kt
@@ -1,10 +1,13 @@
 package com.example.rememberapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -12,9 +15,9 @@ import androidx.navigation.fragment.navArgs
 import com.example.rememberapp.data.PriorityLevel
 import com.example.rememberapp.data.Task
 import com.example.rememberapp.databinding.TaskListAddItemBinding
-import com.example.rememberapp.databinding.TaskListFragmentBinding
 import com.example.rememberapp.viewmodel.TaskListViewModel
 import com.example.rememberapp.viewmodel.TaskListViewModelFactory
+
 
 class TaskListAddModifyItem : Fragment() {
 
@@ -62,6 +65,14 @@ class TaskListAddModifyItem : Fragment() {
 
             binding.radioGroupTaskPriority.check(binding.radioPriorityHigh.id)
             binding.radioGroupTaskPriority.jumpDrawablesToCurrentState()
+        }
+
+        // Hides keyboard when taskName (EditText) is no longer focused.
+        binding.taskName.setOnFocusChangeListener { _, hasFocus ->
+            if(!hasFocus) {
+                val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            }
         }
     }
 
