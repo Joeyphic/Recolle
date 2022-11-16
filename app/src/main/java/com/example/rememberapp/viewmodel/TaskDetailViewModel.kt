4973cfebc72faaf91bc475ae4b58040d9d15698c
Joeyphic <112992779+Joeyphic@users.noreply.github.com>
2022-11-15 23:48:09 -0500

    Comment TaskDetailViewModel

--- app/src/main/java/com/example/rememberapp/viewmodel/TaskDetailViewModel.kt
+++ app/src/main/java/com/example/rememberapp/viewmodel/TaskDetailViewModel.kt
@@ -7,14 +7,32 @@ import kotlinx.coroutines.launch
 
 class TaskDetailViewModel(private val taskDao: TaskDao) : ViewModel() {
 
+    /*
+    ----------------------------------------------------
+    Parameters:   id (Int)
+    Returns:      LiveData<Task?>
+    Description:  -Retrieves the task from the database, using
+                  its ID as identification.
+                  -Returns LiveData since it will be used to
+                  display updated data to views.
+    ----------------------------------------------------
+    */
+    fun retrieveTask(id: Int): LiveData<Task?> {
+        return taskDao.getTaskById(id).asLiveData()
+    }
+
+    /*
+    ----------------------------------------------------
+    Parameters:   task (Task)
+    Description:  -Deletes the task from the database.
+                  -The task affected should be the same one
+                  obtained from retrieveTask() earlier.
+    ----------------------------------------------------
+    */
     fun deleteTask(task: Task) {
         viewModelScope.launch {
             taskDao.deleteTask(task)
         }
-    }
-    
-    fun retrieveTask(id: Int): LiveData<Task?> {
-        return taskDao.getTaskById(id).asLiveData()
     }
 
 }
