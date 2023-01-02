044650a09d24ffb07b697b01b50816e95e53dd0a
Joeyphic <112992779+Joeyphic@users.noreply.github.com>
2023-01-02 15:36:38 -0500

    Comment TaskEditFragment

--- app/src/main/java/com/example/rememberapp/TaskEditFragment.kt
+++ app/src/main/java/com/example/rememberapp/TaskEditFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -21,7 +20,6 @@ import com.example.rememberapp.viewmodel.TaskEditViewModelFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.lang.IllegalStateException
 
 class TaskEditFragment : Fragment() {
 
@@ -46,22 +44,35 @@ class TaskEditFragment : Fragment() {
         return fragmentBinding.root
     }
 
+    /*
+    ----------------------------------------------------
+    Parameters:   view (View), savedInstanceState (Bundle?)
+    Description:  -First, we record the navigation argument "taskId". Then, we use it as a parameter
+                   for the bind() function, which stores the Task in the viewModel.task
+                   variable, and also configures the views to represent the Task.
+                  -If no Task exists which has the same ID as the one in the navigation argument,
+                   then we simply go back to TaskListFragment without making changes.
+                  -Lastly, we hide keyboard when EditText is no longer focused. This is
+                   intended to give room for the user to look at their changes before
+                   finalizing them.
+    ----------------------------------------------------
+    */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val id = navigationArgs.taskId
 
-        // If there's an ID, link selected task to fragment.
+        // If ID exists, bind Task to fragment.
         if(id > 0) {
             bind(id)
         }
-        // Otherwise, there has been invalid input and we navigate back to the Task List.
+        // Otherwise, exit prematurely.
         else {
             val action = TaskEditFragmentDirections.actionTaskEditFragmentToTaskListFragment()
             findNavController().navigate(action)
         }
 
-        // Hides keyboard when taskName (EditText) is no longer focused.
+        // Hide keyboard if EditText is not focused.
         binding.taskName.setOnFocusChangeListener { _, hasFocus ->
             if(!hasFocus) {
                 val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
@@ -70,24 +81,24 @@ class TaskEditFragment : Fragment() {
         }
     }
 
-    private fun isEntryValid(): Boolean {
-        val currentPriority = getPriorityFromRadioId() ?: return false
-        return viewModel.isEntryValid(binding.taskName.text.toString(), currentPriority)
-    }
-
-    private fun getPriorityFromRadioId() : PriorityLevel? {
-        return when (binding.radioGroupTaskPriority.checkedRadioButtonId) {
-            binding.radioPriorityHigh.id -> PriorityLevel.HIGH
-            binding.radioPriorityMedium.id -> PriorityLevel.MEDIUM
-            binding.radioPriorityLow.id -> PriorityLevel.LOW
-            else -> null
-        }
-    }
-
-    // TODO: Separate into two functions. Too much happening at once.
+    /*
+    ----------------------------------------------------
+    Parameters:   taskId (Int)
+    Description:  -Uses the IO thread to obtain the Task corresponding to parameter taskId.
+                  -We use the currentTask variable as an intermediary. When we move back to the
+                   main thread, then we can insert its value into viewModel.task without worrying
+                   about race conditions.
+                  -Next, we populate the EditText and radio buttons with the Task's
+                   values. This shows the user its details before any edits are made.
+                  -If isEntryValid() is true, then the Views have already been populated with
+                   values. This would happen during configuration changes. Therefore, we should not
+                   change the Views, as we'd be overwriting previous changes the user made.
+    ----------------------------------------------------
+    */
     private fun bind(taskId: Int) {
 
         lifecycleScope.launch(Dispatchers.IO) {
+            // Retrieve Task using taskId
             val currentTask = viewModel.retrieveTask(taskId)
 
             withContext(Dispatchers.Main) {
@@ -97,13 +108,14 @@ class TaskEditFragment : Fragment() {
                     findNavController().navigate(action)
                 }
                 else {
+                    // Initialize viewModel.task
                     viewModel.task = currentTask
                     binding.buttonSave.setOnClickListener { updateTask() }
 
-                    // EditText and RadioButton config changes are saved. Since the fragment's
-                    // default state is an invalid input, that means it's safe to override.
+                    // Return early if Views already populated.
                     if(isEntryValid()) return@withContext
 
+                    // Populate views with Task details.
                     binding.apply {
                         layoutTaskName.isHintAnimationEnabled = false
                         taskName.setText(viewModel.task.taskName, TextView.BufferType.SPANNABLE)
@@ -120,6 +132,41 @@ class TaskEditFragment : Fragment() {
         }
     }
 
+    /*
+    ----------------------------------------------------
+    Returns:      Boolean
+    Description:  -Checks if priority RadioButton is selected, then sends to viewModel
+                   to determine validity of both fields.
+    ----------------------------------------------------
+    */
+    private fun isEntryValid(): Boolean {
+        val currentPriority = getPriorityFromRadioId() ?: return false
+        return viewModel.isEntryValid(binding.taskName.text.toString(), currentPriority)
+    }
+
+    /*
+    ----------------------------------------------------
+    Returns:      PriorityLevel?
+    Description:  -Returns the PriorityLevel corresponding to which radio button
+                   in the fragment is selected. Returns null if none are selected.
+    ----------------------------------------------------
+    */
+    private fun getPriorityFromRadioId() : PriorityLevel? {
+        return when (binding.radioGroupTaskPriority.checkedRadioButtonId) {
+            binding.radioPriorityHigh.id -> PriorityLevel.HIGH
+            binding.radioPriorityMedium.id -> PriorityLevel.MEDIUM
+            binding.radioPriorityLow.id -> PriorityLevel.LOW
+            else -> null
+        }
+    }
+
+    /*
+    ----------------------------------------------------
+    Description:  -If isEntryValid() returns true, then the Task would still be valid even
+                   after updating. Therefore, it is sent to the ViewModel to perform the update.
+                  -Afterwards, we return to TaskListFragment.
+    ----------------------------------------------------
+    */
     private fun updateTask() {
         if(isEntryValid()) {
             val updatedPriority = getPriorityFromRadioId() ?: return // Since valid, is non-null
@@ -139,6 +186,7 @@ class TaskEditFragment : Fragment() {
         }
     }
 
+    // Sets _binding to null, avoiding memory leaks.
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
