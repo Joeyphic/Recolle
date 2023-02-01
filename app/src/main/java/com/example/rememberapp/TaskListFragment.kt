7e84423335fc67ccfae6e26f0310ce2fb3ecb037
Joeyphic <112992779+Joeyphic@users.noreply.github.com>
2023-02-01 02:01:35 -0500

    Comment TaskListFragment

--- app/src/main/java/com/example/rememberapp/TaskListFragment.kt
+++ app/src/main/java/com/example/rememberapp/TaskListFragment.kt
@@ -37,9 +37,22 @@ class TaskListFragment : Fragment() {
         return fragmentBinding.root
     }
 
+    /*
+    ----------------------------------------------------
+    Parameters:   view (View), savedInstanceState (Bundle?)
+    Description:  -First, the adapter is initialized and linked to the RecyclerView. The ViewModel
+                   persists throughout the app, so we can call upon a previous state of the list
+                   (recordedTaskList) in order to show changes made by the user in other fragments.
+                  -Next, the FAB button is configured. Most notably, its height and padding is used
+                   to influence the RecyclerView's padding, so it is never covering any details.
+                  -Lastly, the ItemTouchHelper is attached to the RecyclerView, which allows the
+                   user to drag TaskList items.
+    ----------------------------------------------------
+    */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Initialize Adapter
         val adapter = TaskListAdapter {
             val action = TaskListFragmentDirections.actionTaskListFragmentToTaskDetailFragment(it.id)
             this.findNavController().navigate(action)
@@ -61,21 +74,23 @@ class TaskListFragment : Fragment() {
             viewModel.recordedTaskList = tasks
         }
 
+        // Configure FAB
         binding.floatingActionButton.doOnLayout {
             binding.recyclerView.setPadding(0,0,0,
                 (it.paddingBottom + it.measuredHeight)
             )
         }
 
-        val itemTouchHelper = ItemTouchHelper(TaskListAdapter.TaskTouchHelper(adapter) { taskId, to ->
-            viewModel.moveTaskPosition(taskId, to)
-        })
-        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
-
         binding.floatingActionButton.setOnClickListener {
             val action = TaskListFragmentDirections.actionTaskListFragmentToTaskAddFragment()
             this.findNavController().navigate(action)
         }
+
+        // Attach ItemTouchHelper
+        val itemTouchHelper = ItemTouchHelper(TaskListAdapter.TaskTouchHelper(adapter) { taskId, to ->
+            viewModel.moveTaskPosition(taskId, to)
+        })
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
     }
 
     // Sets _binding to null, avoiding memory leaks.
