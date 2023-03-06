d0aa29353f0e1c0ab5e8c059c606db206b4f0d1f
Joeyphic <112992779+Joeyphic@users.noreply.github.com>
2023-03-06 11:47:27 -0500

    Add TypeConverters to Database
    Currently used to convert between LocalDateTime and Long (unix time) in order to store in the database.

---
+++ app/src/main/java/com/example/rememberapp/data/Converters.kt
@@ -0,0 +1,22 @@
+package com.example.rememberapp.data
+
+import androidx.room.TypeConverter
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): LocalDateTime? {
+        return value?.let {
+            // ZoneId.systemDefault() preferred over TimeZone.getDefault().toZoneId()
+            LocalDateTime.ofInstant(Instant.ofEpochSecond(it), ZoneId.systemDefault())
+        }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: LocalDateTime?): Long? {
+        val deviceTimeOffset = ZoneId.systemDefault().rules.getOffset(Instant.now())
+        return date?.toEpochSecond(deviceTimeOffset)
+    }
+}
